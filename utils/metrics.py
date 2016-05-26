086d0bc437e3ed9fbb73c4dd9208a8e0998526a4
Tristan Deleu <tristan.deleu@gmail.com>
2016-05-26 15:34:28 +0200

    Simplify the accuracy metric

--- utils/metrics.py
+++ utils/metrics.py
@@ -6,15 +6,15 @@ import numpy as np
 # predictions is the argmax of the posterior
 def accuracy_instance(predictions, targets, n=[1, 2, 3, 4, 5, 10], \
         nb_classes=5, nb_samples_per_class=10):
-    accuracy_0 = theano.shared(np.zeros((nb_classes, nb_samples_per_class), \
+    accuracy_0 = theano.shared(np.zeros(nb_samples_per_class, \
         dtype=theano.config.floatX))
     indices_0 = theano.shared(np.zeros(nb_samples_per_class, dtype=np.int32))
     def step_(p, t, acc, idx):
-        acc = T.inc_subtensor(acc[t, idx[t]], T.eq(p, t))
+        acc = T.inc_subtensor(acc[idx[t]], T.eq(p, t))
         idx = T.inc_subtensor(idx[t], 1)
         return (acc, idx)
     (raw_accuracy, _), _ = theano.foldr(step_, sequences=[predictions, targets], \
         outputs_info=[accuracy_0, indices_0])
-    accuracy = T.mean(raw_accuracy, axis=0)
+    accuracy = raw_accuracy / nb_classes
 
     return accuracy
