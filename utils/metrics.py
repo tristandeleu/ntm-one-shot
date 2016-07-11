fd7b5a866424d6038b975752db29cae76ea3fbc0
Tristan Deleu <tristan.deleu@gmail.com>
2016-07-11 13:11:45 +0200

    Changed the accuracy computation to use batch size

--- utils/metrics.py
+++ utils/metrics.py
@@ -5,16 +5,17 @@ import numpy as np
 
 # predictions is the argmax of the posterior
 def accuracy_instance(predictions, targets, n=[1, 2, 3, 4, 5, 10], \
-        nb_classes=5, nb_samples_per_class=10):
-    accuracy_0 = theano.shared(np.zeros(nb_samples_per_class, \
+        nb_classes=5, nb_samples_per_class=10, batch_size=1):
+    accuracy_0 = theano.shared(np.zeros((batch_size, nb_samples_per_class), \
         dtype=theano.config.floatX))
-    indices_0 = theano.shared(np.zeros(nb_samples_per_class, dtype=np.int32))
+    indices_0 = theano.shared(np.zeros((batch_size, nb_samples_per_class), \
+        dtype=np.int32))
     def step_(p, t, acc, idx):
-        acc = T.inc_subtensor(acc[idx[t]], T.eq(p, t))
-        idx = T.inc_subtensor(idx[t], 1)
+        acc = T.inc_subtensor(acc[T.arange(batch_size), idx[T.arange(batch_size), t]], T.eq(p, t))
+        idx = T.inc_subtensor(idx[T.arange(batch_size), t], 1)
         return (acc, idx)
-    (raw_accuracy, _), _ = theano.foldr(step_, sequences=[predictions, targets], \
-        outputs_info=[accuracy_0, indices_0])
-    accuracy = raw_accuracy / nb_classes
+    (raw_accuracy, _), _ = theano.foldr(step_, sequences=[predictions.dimshuffle(1, 0), \
+        targets.dimshuffle(1, 0)], outputs_info=[accuracy_0, indices_0])
+    accuracy = T.mean(raw_accuracy / nb_classes, axis=0)
 
     return accuracy
