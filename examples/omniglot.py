4f0e10dd6306afac1c4a388f769a613d40abb27a
Tristan Deleu <tristan.deleu@gmail.com>
2016-08-26 10:26:35 +0200

    Fix omniglot example parameters to rely on the generator

--- examples/omniglot.py
+++ examples/omniglot.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import theano
 import theano.tensor as T
 import numpy as np
@@ -13,21 +15,25 @@ def omniglot():
     input_var = T.tensor3('input') # input_var has dimensions (batch_size, time, input_dim)
     target_var = T.imatrix('target') # target_var has dimensions (batch_size, time) (label indices)
 
+    # Load data
+    generator = OmniglotGenerator(data_folder='./data/omniglot', batch_size=16, \
+        nb_samples=5, nb_samples_per_class=10, max_rotation=0., max_shift=0, max_iter=None)
+
     output_var, output_var_flatten, params = memory_augmented_neural_network(input_var, \
-        target_var, batch_size=16, nb_class=5, memory_shape=(128, 40), \
-        controller_size=200, input_size=20 * 20, nb_reads=4)
+        target_var, batch_size=generator.batch_size, nb_class=generator.nb_samples, \
+        memory_shape=(128, 40), controller_size=200, input_size=20 * 20, nb_reads=4)
 
     cost = T.mean(T.nnet.categorical_crossentropy(output_var_flatten, target_var.flatten()))
     updates = lasagne.updates.adam(cost, params, learning_rate=1e-3)
 
-    accuracies = accuracy_instance(T.argmax(output_var, axis=2), target_var, batch_size=1)
+    accuracies = accuracy_instance(T.argmax(output_var, axis=2), target_var, batch_size=generator.batch_size)
 
+    print('Compiling the model...')
     train_fn = theano.function([input_var, target_var], cost, updates=updates)
     accuracy_fn = theano.function([input_var, target_var], accuracies)
+    print('Done')
 
-    # Load data
-    generator = OmniglotGenerator(data_folder='./data/omniglot', batch_size=16, \
-        nb_samples=5, nb_samples_per_class=10, max_rotation=0., max_shift=0, max_iter=None)
+    print('Training...')
     t0 = time.time()
     all_scores, scores, accs = [], [], np.zeros(generator.nb_samples_per_class)
     try:
@@ -38,11 +44,11 @@ def omniglot():
             scores.append(score)
             accs += acc
             if i > 0 and not (i % 100):
-                print 'Episode %05d: %.6f' % (i, np.mean(score))
-                print accs / 100.
+                print('Episode %05d: %.6f' % (i, np.mean(score)))
+                print(accs / 100.)
                 scores, accs = [], np.zeros(generator.nb_samples_per_class)
     except KeyboardInterrupt:
-        print time.time() - t0
+        print(time.time() - t0)
         pass
 
 
--- mann/utils/metrics.py
+++ mann/utils/metrics.py
@@ -8,11 +8,12 @@ def accuracy_instance(predictions, targets, n=[1, 2, 3, 4, 5, 10], \
         nb_classes=5, nb_samples_per_class=10, batch_size=1):
     accuracy_0 = theano.shared(np.zeros((batch_size, nb_samples_per_class), \
         dtype=theano.config.floatX))
-    indices_0 = theano.shared(np.zeros((batch_size, nb_samples_per_class), \
+    indices_0 = theano.shared(np.zeros((batch_size, nb_classes), \
         dtype=np.int32))
+    batch_range = T.arange(batch_size)
     def step_(p, t, acc, idx):
-        acc = T.inc_subtensor(acc[T.arange(batch_size), idx[T.arange(batch_size), t]], T.eq(p, t))
-        idx = T.inc_subtensor(idx[T.arange(batch_size), t], 1)
+        acc = T.inc_subtensor(acc[batch_range, idx[batch_range, t]], T.eq(p, t))
+        idx = T.inc_subtensor(idx[batch_range, t], 1)
         return (acc, idx)
     (raw_accuracy, _), _ = theano.foldl(step_, sequences=[predictions.dimshuffle(1, 0), \
         targets.dimshuffle(1, 0)], outputs_info=[accuracy_0, indices_0])
